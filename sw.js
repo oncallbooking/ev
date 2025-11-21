8fe3bad79df7970e6bbc206391220b553aa0b37d
oncallbooking <143792222+oncallbooking@users.noreply.github.com>
2025-11-21 15:10:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,6 @@
-// Simple service worker for caching
 const CACHE = 'futrifix-v1';
-const toCache = ['/', '/index.html', '/app.css', '/app.js'];
-
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(toCache)));
-});
-
-self.addEventListener('fetch', e=>{
+const ASSETS = ['index.html','styles.css','main.js','manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
 });
